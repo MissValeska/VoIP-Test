efd2028c2599396cc0a343d427238c730b88c6ff
MissValeska <missvaleskavv@gmail.com>
2016-04-11 16:17:06 -0700

    This should be ready for use; Yet to be extensively tested.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 clang VoIPTestMain.c -o VoIPTestMain -g SendRecv.c PulseAudioMicInput.c -lpulse-simple -lpulse -lpthread
-=======
-clang VoIPTestMain.c -o VoIPTestMain -g -L. VoIPSendRecv.c PulseAudioMicInput.c -lpulse-simple -lpulse -lpthread
->>>>>>> a4150c9c0e81a09740cd44a5eabad1664830588b
